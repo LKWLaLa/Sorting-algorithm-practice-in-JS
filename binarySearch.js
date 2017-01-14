ea1638d5e70443fd3f1d6ad5fd035e5dcfd05350
LKWLaLa <lkwlala@gmail.com>
2017-01-13 21:18:16 -0500

    refactor control flow

--- binarySearch.js
+++ binarySearch.js
@@ -1,31 +1,27 @@
 var sortedInput = [2, 6, 7, 8, 10, 12, 15, 20, 30, 55]
 
 function binarySearch(item, input, start, end){
-    if(end - start == 0){
-      if(item == input[start]){
-        return start;
-      }
-      if(item != input[start]){
-        return false;
-      }
+  if(end - start == 0){
+    if(item != input[start]){
+      return false;
     }
-    else{
-      var mid = Math.floor(((end - start)/2) + start);
+    return start;
+  }
+  
+  var mid = Math.floor(((end - start)/2) + start);
 
-      if(input[mid] < item){
-        start = mid + 1
-        return binarySearch(item, input, start, end);
-      };
+  if(input[mid] < item){
+    start = mid + 1;
+    return binarySearch(item, input, start, end);
+  }
 
-      if(input[mid] > item){
-        end = mid - 1
-        return binarySearch(item, input, start, end);
-        // MUST use return statement here, or ultimate return value will be undefined.
-      };
-        return mid
-    };
-
-};
+  if(input[mid] > item){
+    end = mid - 1;
+    return binarySearch(item, input, start, end);
+    // MUST use return statement here, or ultimate return value will be undefined.
+  }
+  return mid;
+}
 
 console.log(binarySearch(55, sortedInput, 0, (sortedInput.length - 1)))
 
